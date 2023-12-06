a79647b3a0e09e12294edde59e3be871415e810f
Harshgupta2902 <147129512+Harshgupta2902@users.noreply.github.com>
2023-12-06 17:15:18 +0530

    navbar search added

--- application/views/Assets/headers.php
+++ application/views/Assets/headers.php
@@ -36,3 +36,6 @@
 <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 <script src="https://cdn.tailwindcss.com"></script>
 <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
--- application/views/Assets/topbar.php
+++ application/views/Assets/topbar.php
@@ -1,20 +1,30 @@
-<div section="nav" class="c-siteHeader g-outer-spacing-bottom-large">
-        <div class="c-siteHeader_grid g-grid-container">
-          <div section="logo" class="c-siteLogo"><a href="/" rel="follow" aria-label="Download.com"><svg
-                class="c-siteLogo_image c-siteLogo_image-medium g-fill-white">
-                <use xlink:href="#logo"></use>
-              </svg></a></div>
-          <div class="c-searchBox_autoSuggest">
-            <form><button type="submit" class="c-searchBox_autoSuggest_button"><svg
-                  class="c-searchBox_autoSuggest_magGlass">
-                  <use xlink:href="#mag"></use>
-                </svg></button>
-              <div class="c-searchBox_autoSuggest_inputBox"></div> <input id="c-searchBox_autoSuggest_id" type="text"
-                placeholder="Search Apps..." value="" class="c-searchBox_autoSuggest_inputElement">
-            </form>
+
+<div class="antialiased bg-gray-100 dark-mode:bg-gray-900">
+  <div class="w-full text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800">
+    <div x-data="{ open: true }" class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+      <div class="flex flex-row items-center justify-between p-4">
+        <a href="#" class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline">Online Download</a>
+        
+        
+        
+        <button class="rounded-lg md:hidden focus:outline-none focus:shadow-outline" @click="open = !open">
+          <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
+            <path x-show="!open" fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+            <path x-show="open" fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+          </svg>
+        </button>
+      </div>
+      
+      <nav :class="{'flex': open, 'hidden': !open}" class="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
+    <div x-data="{ openSearch: false }" class="md:flex md:items-center md:justify-end">
+            <!-- Search field for desktop -->
+            <div class=" md:block">
+                <input type="text" class="p-2 border rounded" placeholder="Search...">
+            </div>
         </div>
-        <nav class="flex justify-center">
-            <ul class="flex flex-wrap items-center font-medium text-sm">
+
+
+        <ul class="flex flex-wrap items-center font-medium text-sm">
                 <?php foreach ($mainNav as $mainNav): ?>
                     <li class="p-4 lg:px-8 relative flex items-center space-x-1" x-data="{ open: false }"
                     @mouseenter="open = true" @mouseleave="open = false">
@@ -44,13 +54,12 @@
                                         <span class="whitespace-nowrap"><?php echo $subnavItem["subnav_title"]; ?></span>
                                     </a>
                                 </li>
-                            <?php
-    endforeach; ?>
+                            <?php endforeach; ?>
                         </ul>
                     </li>
-                <?php
-endforeach; ?>
+                <?php endforeach; ?>
             </ul>
         </nav>
-</div>
-</div>
+        </div>
+    </div>
+    </div>
